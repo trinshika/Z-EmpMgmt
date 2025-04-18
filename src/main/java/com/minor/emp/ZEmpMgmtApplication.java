8c101904c5f8f0474170ffc60481608e7bcdb35b
Trinshika Yadav <trinshika@gmail.com>
2025-04-19 01:13:26 +0530

    first commit

---
+++ src/main/java/com/minor/emp/ZEmpMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.minor.emp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZEmpMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZEmpMgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minor/emp/ZEmpMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minor.emp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZEmpMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
